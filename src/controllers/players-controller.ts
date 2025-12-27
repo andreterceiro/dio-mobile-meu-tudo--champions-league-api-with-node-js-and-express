a7d6cc0f2fb5857e21a3425bdca545d9413428c9
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-27 19:10:53 -0300

    feat: removing a commented line and making a fake response to a test

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -5,12 +5,15 @@ import { PlayerModel } from "../models/player-model";
 export async function getPlayer(req: Request, res: Response) {
     const data = await getPlayerDataService();
     res.status(data.statusCode).json(data.body);
-}
+};
 
 export async function getPlayerById(req: Request, res: Response) {
     const id = parseInt(req.params.id);
     const data = await getPlayerByIdService(id);
-    // console.log(data);
 
     res.status(data.statusCode).json(data.body);
+};
+
+export async function insertPlayer(req: Request, res: Response) {
+    res.status(200).json({"ok": "ok"});
 }
