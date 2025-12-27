c40ef1a9513625d15b98ad39c6649d550c455955
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 23:41:22 -0300

    feat: creating a '/' 'hello world' endpoint in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello world");
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.get("/", (request: Request, response: Response) => {
+    response.send("Hello world");
+})
+
+app.listen(process.env.PORT);
+console.log("end of server.ts");
+console.log(process.env.PORT)
