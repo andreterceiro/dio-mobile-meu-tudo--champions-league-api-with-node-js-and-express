052dd728f70404ef202c36891b5c9bc9195301f6
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-27 17:27:43 -0300

    feat: creating a route to insert a player

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import {getPlayer, getPlayerById} from "./controllers/players-controller";
+import {getPlayer, getPlayerById, insertPlayer} from "./controllers/players-controller";
 
 const router = Router();
 
 router.get('/players', getPlayer);
 router.get('/players/:id', getPlayerById);
+router.post('/players', insertPlayer);
+
 
 export default router;
