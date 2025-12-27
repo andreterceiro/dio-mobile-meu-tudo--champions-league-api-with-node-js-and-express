7243f1321198203cb813849c1ea62c5e846629dc
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-27 17:24:14 -0300

    adjustinf imports, the getPlayer function and creating a function in the controllers layer that returns a player based on its ID

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,9 +1,16 @@
 import {Request, Response} from "express";
-import { getPlayerData } from "../services/players-service";
+import { getPlayerDataService, getPlayerByIdService } from "../services/players-service";
+import { PlayerModel } from "../models/player-model";
 
-async function getPlayer(req: Request, res: Response) {
-    const data = await getPlayerData();
+export async function getPlayer(req: Request, res: Response) {
+    const data = await getPlayerDataService();
     res.status(data.statusCode).json(data.body);
 }
 
-export default getPlayer;
+export async function getPlayerById(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    const data = await getPlayerByIdService(id);
+    // console.log(data);
+
+    res.status(data.statusCode).json(data.body);
+}
