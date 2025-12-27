1ba31b4c542fb3b88fdaec274b144938fe69166e
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-27 14:01:01 -0300

    feat: findAllPlayers function in src/repositories/players-repository.ts

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -7,3 +7,7 @@ const database: PlayerModel[] = [
     {id: 1, name: "Messi"},
     {id: 2, name: "Ronaldo"}
 ];
+
+export const findAllPlayers = async (): Promise<PlayerModel[]> => {
+    return database;
+}
