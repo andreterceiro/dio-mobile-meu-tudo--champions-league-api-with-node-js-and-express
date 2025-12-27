8850c20602b3db231933c77592a36e39d728508b
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-27 13:49:36 -0300

    feat: adjusting the controller to match the service adjusts

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,11 +1,10 @@
 import express, {Request, Response} from "express";
 import { getPlayerData } from "../services/players-service";
-import { ok } from "../utils/http-helper";
+import { HttpResponse } from "../utils/http-helper";
 
 async function getPlayer(req: Request, res: Response) {
     const data = await getPlayerData();
-    const response = await ok(data);
-    res.status(response.statusCode).json(response.body);
+    res.status(data.statusCode).json(data.body);
 }
 
 export default getPlayer;
