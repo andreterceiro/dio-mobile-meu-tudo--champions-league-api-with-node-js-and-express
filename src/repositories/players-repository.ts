6a822b86c1c9a570c6e3d5e0223a75e6d2b5a751
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-27 16:37:34 -0300

    feat: changing the data to obey the interface PlayerModel

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -1,9 +1,39 @@
 import { PlayerModel } from "../models/player-model";
 
 const database: PlayerModel[] = [
-    {id: 1, name: "Messi"},
-    {id: 2, name: "Ronaldo"}
-];
+    {
+        "id": 1,
+        "name": "Lionel Messi",
+        "club": "Inter Miami",
+        "nationality": "Argentina",
+        "position": "Forward",
+        "statistics": {
+            "Overall": 93,
+            "Pace": 80,
+            "Shooting": 92,
+            "Passing": 91,
+            "Dribbling": 95,
+            "Defending": 34,
+            "Physical": 65
+        }
+    },
+    {
+        "id": 2,
+        "name": "Cristiano Ronaldo",
+        "club": "Al Nassr",
+        "nationality": "Portugal",
+        "position": "Forward",
+        "statistics": {
+            "Overall": 88,
+            "Pace": 81,
+            "Shooting": 92,
+            "Passing": 78,
+            "Dribbling": 84,
+            "Defending": 34,
+            "Physical": 75
+        }
+    },
+]
 
 export const findAllPlayers = async (): Promise<PlayerModel[]> => {
     return database;
