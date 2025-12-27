a82e9bcc86e756af372500eaea4ca3b675e54ad1
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-27 10:35:52 -0300

    feat: app.use(express.json()) in src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from "express";
 
 function createApp() {
     const app = express();
+    app.use(express.json());
 
     app.get("/", (request: Request, response: Response) => {
         response.send("Hello world");
