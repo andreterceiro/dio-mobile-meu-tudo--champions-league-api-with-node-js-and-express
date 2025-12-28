beb2c6d66763e1eea03953bc003248dd798e7913
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-28 12:12:12 -0300

    feat: creating a route to list all clubs (teams)

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import {getPlayer, getPlayerById, insertPlayer, deletePlayerById, updatePlayerById} from "./controllers/players-controller";
+import {getClubs} from "./controllers/clubs-controller";
 
 const router = Router();
 
@@ -8,5 +9,7 @@ router.get('/players/:id', getPlayerById);
 router.post('/players', insertPlayer);
 router.delete('/players/:id', deletePlayerById);
 router.put('/players/:id', updatePlayerById);
+
+router.get('/clubs', getClubs);
 
 export default router;
