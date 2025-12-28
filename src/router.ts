2bd106131c2b42887168468796b0a2f7b29b6952
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-28 10:03:59 -0300

    feat: creating a route to update a player

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {getPlayer, getPlayerById, insertPlayer, deletePlayerById} from "./controllers/players-controller";
+import {getPlayer, getPlayerById, insertPlayer, deletePlayerById, updatePlayerById} from "./controllers/players-controller";
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.get('/players', getPlayer);
 router.get('/players/:id', getPlayerById);
 router.post('/players', insertPlayer);
 router.delete('/players/:id', deletePlayerById);
+router.patch('/players/:id', updatePlayerById);
 
 export default router;
