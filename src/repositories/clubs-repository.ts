8c1b56ab60ceb0974a5aaaf154f3d308f73e06c6
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-28 12:30:36 -0300

    feat: now clubs-repository.json uses an external JSON file

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,24 +1,7 @@
 import { ClubModel } from "../models/club-model";
+import fs from "fs";
 
-let database: ClubModel[] = [
-    {
-        "id": 1,
-        "name": "Corinthians",
-    },
-    {
-        "id": 2,
-        "name": "Palmeiras",
-    },
-    {
-        "id": 3,
-        "name": "Santos",
-    },
-    {
-        "id": 4,
-        "name": "São Paulo",
-    }
-]
-
+let database: ClubModel[] = JSON.parse(fs.readFileSync(__dirname + "/../data/clubs.json").toString());
 export const findAllClubs = async (): Promise<ClubModel[]> => {
     return database;
 }
