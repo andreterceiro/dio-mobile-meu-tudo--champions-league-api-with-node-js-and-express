9c62f8215d5093c89d02d3faea116e286c0b61c3
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-28 08:42:28 -0300

    feat: implementing a route to delete a player

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
 import {Router} from "express";
-import {getPlayer, getPlayerById, insertPlayer} from "./controllers/players-controller";
+import {getPlayer, getPlayerById, insertPlayer, deletePlayerById} from "./controllers/players-controller";
 
 const router = Router();
 
 router.get('/players', getPlayer);
 router.get('/players/:id', getPlayerById);
 router.post('/players', insertPlayer);
-
+router.delete('/players/:id', deletePlayerById);
 
 export default router;
