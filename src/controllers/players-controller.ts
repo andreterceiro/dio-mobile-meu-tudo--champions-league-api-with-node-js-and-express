0fc9af10e49581499fa3511b70e27e71e8b9a415
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-28 08:43:17 -0300

    feat:implementing the controller method to delete a player

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from "express";
-import { insertPlayerService, getPlayerDataService, getPlayerByIdService } from "../services/players-service";
+import { insertPlayerService, getPlayerDataService, getPlayerByIdService, deletePlayerByIdService } from "../services/players-service";
 
 export async function getPlayer(req: Request, res: Response) {
     const data = await getPlayerDataService();
@@ -15,5 +15,11 @@ export async function getPlayerById(req: Request, res: Response) {
 
 export async function insertPlayer(req: Request, res: Response) {
     const data = await insertPlayerService(req.body);
+    res.status(data.statusCode).json(data.body);
+}
+
+export async function deletePlayerById(req: Request, res: Response) {
+    const id = parseInt(req.params.id);
+    const data = await deletePlayerByIdService(id);
     res.status(data.statusCode).json(data.body);
 }
