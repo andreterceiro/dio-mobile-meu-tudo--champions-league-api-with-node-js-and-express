22604afbada9ab811708243920d5e060c8b05085
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-28 12:42:32 -0300

    feat: basic use of the package 'cors'

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import router from "./router";
+import cors from "cors";
 
 function createApp() {
     const app = express();
     app.use(express.json());
-
     app.use('/api', router);
+    app.use(cors());
 
     return app;
 }
